65a1f8b7e278248d787af19737c4a67039291102
NeerajYadu07 <neerajyadu8281@gmail.com>
2022-04-09 21:01:28 +0530

    my first commit

---
+++ lib/screens/Dictionary.dart
@@ -0,0 +1,151 @@
+import 'package:flutter/material.dart';
+import 'dart:async';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'dart:io';
+
+
+
+
+
+
+class dictionary extends StatefulWidget {
+  dictionary({Key? key}) : super(key: key);
+
+  @override
+  State<dictionary> createState() => _dictionaryState();
+}
+
+class _dictionaryState extends State<dictionary> {
+    String url = "https://owlbot.info/api/v4/dictionary/";
+    String token = "f58d2e2ffc435ea52e9bbe10d9eb9514f37c0a85";
+
+  TextEditingController textEditingController = TextEditingController();
+
+
+  // Stream for loading the text as soon as it is typed
+ late StreamController streamController;
+ late Stream  _stream;
+ late Timer  _debounce;
+
+  // search function
+  searchText() async {
+    if (textEditingController.text == null ||
+        textEditingController.text.length == 0) {
+        streamController.add(null);
+        return;
+    }
+    streamController.add("waiting");
+    http.Response response = await http.get(url + textEditingController.text.trim() as Uri,
+        // do provide spacing after Token
+        headers: {"Authorization": "Token " + token});
+    streamController.add(json.decode(response.body));
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    streamController = StreamController();
+    _stream = streamController.stream;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        title: Text(
+          "Dictionary",
+          style: TextStyle(color: Colors.white),
+        ),
+        centerTitle: true,
+        bottom: PreferredSize(
+          preferredSize: Size.fromHeight(45),
+          child: Row(
+            children: [
+              Expanded(
+                child: Container(
+                  margin: const EdgeInsets.only(left: 12, bottom: 11.0),
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(24.0),
+                      color: Colors.white),
+                  child: TextFormField(            
+                    controller: textEditingController,
+                    decoration: InputDecoration(
+                      hintText: "Search for a word",
+                      contentPadding: const EdgeInsets.only(left: 24.0),
+
+                      // removing the input border
+                      border: InputBorder.none,
+                    ),
+                  ),
+                ),
+              ),
+              IconButton(
+                icon: Icon(
+                  Icons.search,
+                  color: Colors.white,
+                ),
+                onPressed: () {
+                  searchText();
+                },
+              )
+            ],
+          ),
+        ),
+      ),
+      body: Container(
+        margin: EdgeInsets.all(8),
+        child: StreamBuilder(
+          builder: (BuildContext context, AsyncSnapshot snapshot) {
+            if (snapshot.data == null) {
+              return Center(
+                child: Text("Enter a search word"),
+              );
+            }
+            if (snapshot.data == "waiting") {
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+
+            // output
+            return ListView.builder(
+              itemCount: snapshot.data["definitions"].length,
+              itemBuilder: (BuildContext context, int index) {
+                return ListBody(
+                  children: [
+                    Container(
+                      color: Colors.grey[300],
+                      child: ListTile(
+                        leading: snapshot.data["definitions"][index]
+                                    ["image_url"] ==
+                                null
+                            ? null
+                            : CircleAvatar(
+                                backgroundImage: NetworkImage(snapshot
+                                    .data["definitions"][index]["image_url"]),
+                              ),
+                        title: Text(textEditingController.text.trim() +
+                            "(" +
+                            snapshot.data["definitions"][index]["type"] +
+                            ")"),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text(
+                          snapshot.data["definitions"][index]["definition"]),
+                    )
+                  ],
+                );
+              },
+            );
+          },
+          stream: _stream,
+        ),
+      ),
+    );
+  }
+}
+      
---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,120 @@
+import 'package:firstapp/screens/Dictionary.dart';
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firstapp/model/user_model.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'login_screen.dart';
+
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  User? user = FirebaseAuth.instance.currentUser;
+  UserModel loggedInUser = UserModel();
+
+
+  @override
+  
+  void initState() {
+    super.initState();
+    FirebaseFirestore.instance
+        .collection("users")
+        .doc(user!.uid)
+        .get()
+        .then((value) {
+      this.loggedInUser = UserModel.fromMap(value.data());
+      setState(() {});
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Welcome",),
+      ),
+
+      body: Container(
+        padding:EdgeInsets.all(50),
+        child: Row(
+          
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Material(
+      elevation: 5,
+      borderRadius: BorderRadius.circular(5),
+      color: Colors.blueAccent,
+      child: MaterialButton(
+              height: 200,
+              minWidth: 200,
+              onPressed: () {
+                Navigator.of(context).pushReplacement(
+                      MaterialPageRoute(builder: (context) => dictionary()));
+               
+              },
+              child: Text(
+                "DICTIONARY",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                    fontSize: 30, color: Colors.white, fontWeight: FontWeight.bold),
+              )),
+    ),
+          ],
+        ),
+      ),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            DrawerHeader(
+              child: Text("WELCOME"),
+              decoration: BoxDecoration(
+                color: Colors.blue,
+                image: DecorationImage(
+                  image: AssetImage("assets/gold.jpg"),
+                     fit: BoxFit.cover) ),
+            ),
+            ListTile(
+              leading: Icon(Icons.person),
+              title: Text("${loggedInUser.firstName} ${loggedInUser.secondName}",
+                  style: TextStyle(
+                    color: Colors.black54,
+                    fontWeight: FontWeight.w500,
+                  )),
+              subtitle: Text("Personal"),
+              trailing: Icon(Icons.edit),
+              ),
+            ListTile(
+              leading: Icon(Icons.email),
+              title: Text("${loggedInUser.email}",
+                  style: TextStyle(
+                    color: Colors.black54,
+                    fontWeight: FontWeight.w500,
+                  )),
+              
+              trailing: Icon(Icons.send),
+              ),
+            ActionChip(
+                  label: Text("Logout"),
+                  onPressed: () {
+                    logout(context);
+                  }),
+            ],
+         ),
+      ),
+    );
+  }
+
+  // the logout function
+  Future<void> logout(BuildContext context) async {
+    await FirebaseAuth.instance.signOut();
+    Navigator.of(context).pushReplacement(
+        MaterialPageRoute(builder: (context) => LoginScreen()));
+  }
+}  
